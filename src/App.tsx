59097d53193351f853e3f0cce62bfe5db277a594
Soleimani <l.soleimani2000@gmail.com>
2023-05-20 13:50:08 -0700

    feat: implement routing for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-     Hello Charisma
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
